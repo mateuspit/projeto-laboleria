1b408b7a3fc4f3a22e59f816c41de587ae1e1848
Mateus Barcelos <mateuspit@hotmail.com>
2023-06-07 20:13:18 -0300

    fix: accept only url with https or http

--- src/schemas/cakes.schemas.js
+++ src/schemas/cakes.schemas.js
@@ -3,9 +3,7 @@ import Joi from "joi";
 export const cakeSchema = Joi.object({
     name: Joi.string().min(2).required(),
     price: Joi.number().greater(0).precision(2).required(),
-    image: Joi.string().min(1).required().uri({
-        scheme: ['http', 'https'],
-    }),
+    image: Joi.string().min(1).required().uri(),
     description: Joi.string().min(0),
     flavourId: Joi.number().min(1)
 });
