d82e9604e59821a043ade990c0f8b0a797970f2a
Mateus Barcelos <mateuspit@hotmail.com>
2023-06-07 18:31:11 -0300

    deploy config

--- dump.sql
+++ dump.sql
@@ -1,56 +1,366 @@
-CREATE TABLE "public.cakes" (
-	"id" serial NOT NULL,
-	"name" varchar NOT NULL,
-	"price" numeric NOT NULL,
-	"image" varchar NOT NULL,
-	"description" TEXT NOT NULL,
-	"flavourId" integer NOT NULL,
-	CONSTRAINT "cakes_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.8 (Ubuntu 14.8-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.8 (Ubuntu 14.8-0ubuntu0.22.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: cakes; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.cakes (
+    id integer NOT NULL,
+    name character varying NOT NULL,
+    price numeric NOT NULL,
+    image character varying NOT NULL,
+    description text NOT NULL,
+    "flavourId" integer NOT NULL
 );
 
 
+--
+-- Name: cakes_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
 
-CREATE TABLE "public.clients" (
-	"id" serial NOT NULL,
-	"name" varchar NOT NULL,
-	"address" varchar NOT NULL,
-	"phone" varchar NOT NULL,
-	CONSTRAINT "clients_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+CREATE SEQUENCE public.cakes_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: cakes_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.cakes_id_seq OWNED BY public.cakes.id;
+
+
+--
+-- Name: clients; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.clients (
+    id integer NOT NULL,
+    name character varying NOT NULL,
+    address character varying NOT NULL,
+    phone character varying NOT NULL
 );
 
 
+--
+-- Name: clients_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
 
-CREATE TABLE "public.orders" (
-	"id" serial NOT NULL,
-	"clientId" integer NOT NULL,
-	"cakeId" integer NOT NULL,
-	"quantity" integer NOT NULL,
-	"createdAt" TIMESTAMP NOT NULL,
-	"totalPrice" numeric NOT NULL,
-	"isDelivered" BOOLEAN NOT NULL DEFAULT 'false',
-	CONSTRAINT "orders_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+CREATE SEQUENCE public.clients_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: clients_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.clients_id_seq OWNED BY public.clients.id;
+
+
+--
+-- Name: flavours; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.flavours (
+    id integer NOT NULL,
+    name character varying NOT NULL
 );
 
 
+--
+-- Name: flavours_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
 
-CREATE TABLE "public.flavours" (
-	"id" serial NOT NULL,
-	"name" varchar NOT NULL,
-	CONSTRAINT "flavours_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+CREATE SEQUENCE public.flavours_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: flavours_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.flavours_id_seq OWNED BY public.flavours.id;
+
+
+--
+-- Name: orders; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.orders (
+    id integer NOT NULL,
+    "clientId" integer NOT NULL,
+    "cakeId" integer NOT NULL,
+    quantity integer NOT NULL,
+    "createdAt" timestamp without time zone NOT NULL,
+    "totalPrice" numeric NOT NULL,
+    "isDelivered" boolean DEFAULT false NOT NULL
 );
 
 
+--
+-- Name: orders_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
 
-ALTER TABLE "cakes" ADD CONSTRAINT "cakes_fk0" FOREIGN KEY ("flavourId") REFERENCES "flavours"("id");
+CREATE SEQUENCE public.orders_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
 
-ALTER TABLE "orders" ADD CONSTRAINT "orders_fk0" FOREIGN KEY ("clientId") REFERENCES "clients"("id");
-ALTER TABLE "orders" ADD CONSTRAINT "orders_fk1" FOREIGN KEY ("cakeId") REFERENCES "cakes"("id");
+--
+-- Name: orders_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.orders_id_seq OWNED BY public.orders.id;
+
+
+--
+-- Name: cakes id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.cakes ALTER COLUMN id SET DEFAULT nextval('public.cakes_id_seq'::regclass);
+
+
+--
+-- Name: clients id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.clients ALTER COLUMN id SET DEFAULT nextval('public.clients_id_seq'::regclass);
+
+
+--
+-- Name: flavours id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.flavours ALTER COLUMN id SET DEFAULT nextval('public.flavours_id_seq'::regclass);
+
+
+--
+-- Name: orders id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.orders ALTER COLUMN id SET DEFAULT nextval('public.orders_id_seq'::regclass);
+
+
+--
+-- Data for Name: cakes; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.cakes VALUES (1, 'Bolo de goiaba', 13.00, 'encurtador.com.br/iDIX0', 'Não é bolonha', 1);
+INSERT INTO public.cakes VALUES (2, 'Bolo de pote', 13, 'Bolo de chocolate com recheio de leite ninho', 'https://www.encurtador.com.br/', 1);
+INSERT INTO public.cakes VALUES (3, 'Bolo de oreo', 13, 'Bolo de chocolate com recheio de leite ninho', 'https://www.encurtador.com.br/', 1);
+INSERT INTO public.cakes VALUES (4, 'Bolo de murucujá', 1.69, 'Bolo de chocolate com recheio de leite ninho', 'https://www.encurtador.com.br/', 1);
+INSERT INTO public.cakes VALUES (5, 'Bolo de morango', 1.69, '', 'https://www.encurtador.com.br/', 1);
+INSERT INTO public.cakes VALUES (6, 'Bolo de açaí', 1.69, 'descripção', 'https://www.encurtador.com.br/', 1);
+INSERT INTO public.cakes VALUES (7, 'Bolo de felicidade', 1.69, 'descripção', 'https://www.encurtador.com.br/', 1);
+
+
+--
+-- Data for Name: clients; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.clients VALUES (1, 'Fulana', 'Rua tal', '2199999999');
+INSERT INTO public.clients VALUES (2, 'Fulana', 'Rua tal', '21999999999');
+INSERT INTO public.clients VALUES (3, 'Fulana', 'Rua tal', '21699999999');
+
+
+--
+-- Data for Name: flavours; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.flavours VALUES (1, 'goiaba');
+INSERT INTO public.flavours VALUES (2, 'caipirinha');
+INSERT INTO public.flavours VALUES (3, 'Morango');
+INSERT INTO public.flavours VALUES (4, 'uva');
+
+
+--
+-- Data for Name: orders; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.orders VALUES (1, 1, 1, 2, '2022-06-06 14:52:24.332104', 26, false);
+INSERT INTO public.orders VALUES (3, 3, 1, 5, '2023-06-06 16:58:42.697558', 69, true);
+INSERT INTO public.orders VALUES (2, 1, 1, 2, '2023-06-06 14:52:34.999835', 26, true);
+
+
+--
+-- Name: cakes_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.cakes_id_seq', 7, true);
+
+
+--
+-- Name: clients_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.clients_id_seq', 3, true);
+
+
+--
+-- Name: flavours_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.flavours_id_seq', 4, true);
+
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.orders_id_seq', 3, true);
+
+
+--
+-- Name: cakes cakes_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.cakes
+    ADD CONSTRAINT cakes_pk PRIMARY KEY (id);
+
+
+--
+-- Name: clients clients_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.clients
+    ADD CONSTRAINT clients_pk PRIMARY KEY (id);
+
+
+--
+-- Name: flavours flavours_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.flavours
+    ADD CONSTRAINT flavours_pk PRIMARY KEY (id);
+
+
+--
+-- Name: orders orders_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_pk PRIMARY KEY (id);
+
+
+--
+-- Name: cakes cakes_fk0; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.cakes
+    ADD CONSTRAINT cakes_fk0 FOREIGN KEY ("flavourId") REFERENCES public.flavours(id);
+
+
+--
+-- Name: orders orders_fk0; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_fk0 FOREIGN KEY ("clientId") REFERENCES public.clients(id);
+
+
+--
+-- Name: orders orders_fk1; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_fk1 FOREIGN KEY ("cakeId") REFERENCES public.cakes(id);
+
+
+--
+-- Name: TABLE cakes; Type: ACL; Schema: public; Owner: -
+--
+
+GRANT ALL ON TABLE public.cakes TO "boleriaUser";
+
+
+--
+-- Name: SEQUENCE cakes_id_seq; Type: ACL; Schema: public; Owner: -
+--
+
+GRANT ALL ON SEQUENCE public.cakes_id_seq TO "boleriaUser";
+
+
+--
+-- Name: TABLE clients; Type: ACL; Schema: public; Owner: -
+--
+
+GRANT ALL ON TABLE public.clients TO "boleriaUser";
+
+
+--
+-- Name: SEQUENCE clients_id_seq; Type: ACL; Schema: public; Owner: -
+--
+
+GRANT ALL ON SEQUENCE public.clients_id_seq TO "boleriaUser";
+
+
+--
+-- Name: TABLE flavours; Type: ACL; Schema: public; Owner: -
+--
+
+GRANT ALL ON TABLE public.flavours TO "boleriaUser";
+
+
+--
+-- Name: SEQUENCE flavours_id_seq; Type: ACL; Schema: public; Owner: -
+--
+
+GRANT ALL ON SEQUENCE public.flavours_id_seq TO "boleriaUser";
+
+
+--
+-- Name: TABLE orders; Type: ACL; Schema: public; Owner: -
+--
+
+GRANT ALL ON TABLE public.orders TO "boleriaUser";
+
+
+--
+-- Name: SEQUENCE orders_id_seq; Type: ACL; Schema: public; Owner: -
+--
+
+GRANT ALL ON SEQUENCE public.orders_id_seq TO "boleriaUser";
+
+
+--
+-- PostgreSQL database dump complete
+--
+
